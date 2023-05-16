71926af7ff04c8b6bc75ce8c67ffa56e8ab75697
Laxcod <90342920+Laxcod@users.noreply.github.com>
2023-05-16 21:29:51 +0700

    Add Input Touch Screen Test
    Menambahan Test Touch Screen Pada permainan Ini

---
+++ Labyrinth Moonawar/Assets/TouchTest.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.touchCount ==0) 
+       return;
+
+       var touches = Input.touches;
+
+       for (int i = 0; i < touches.Length; i++)
+       {
+        
+         var touch = touches[i];
+
+         switch (touch.phase)
+       {
+            case TouchPhase.Began:
+                Debug.Log("BEgan " + i + "" + touch.position);
+                break;
+            case TouchPhase.Stationary:
+                Debug.Log("Stationary " + i + "" + touch.position);
+                break;
+            case TouchPhase.Moved:
+                Debug.Log("Moved " +i + "" + touch.deltaPosition);
+                break;
+            case TouchPhase.Ended:
+                Debug.Log("Ended " +i + "" + touch.position);
+                break;
+            case TouchPhase.Canceled:
+            Debug.Log("Canceled " +i + "" + touch.position);
+                break;
+       }
+
+       }
+    }
+}
