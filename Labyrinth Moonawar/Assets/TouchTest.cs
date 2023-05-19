97a02db3e39b0e59d55731fe414c54ed87b4ff78
Laxcod <90342920+Laxcod@users.noreply.github.com>
2023-05-19 16:41:07 +0700

    Add Mini Golf Package To Labyrinth
    Menambahkan Asset dari mini golf ke dalam labyrinth ini

--- Labyrinth Moonawar/Assets/TouchTest.cs
+++ Labyrinth Moonawar/Assets/TouchTest.cs
@@ -1,41 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class TouchTest : MonoBehaviour
-{
-    // Update is called once per frame
-    void Update()
-    {
-       if(Input.touchCount ==0) 
-       return;
+// public class TouchTest : MonoBehaviour
+// {
+//     [SerializeField] GameObject visualTemplate;
 
-       var touches = Input.touches;
+//     List<GameObject> visual list = new List<GameObject>();
 
-       for (int i = 0; i < touches.Length; i++)
-       {
+//     void Update()
+//     {
+//        if(Input.touchCount ==0) 
+//        return;
+
+//        var touches = Input.touches;
+
+//        while (visual)
+//        {
         
-         var touch = touches[i];
+//        }
 
-         switch (touch.phase)
-       {
-            case TouchPhase.Began:
-                Debug.Log("BEgan " + i + "" + touch.position);
-                break;
-            case TouchPhase.Stationary:
-                Debug.Log("Stationary " + i + "" + touch.position);
-                break;
-            case TouchPhase.Moved:
-                Debug.Log("Moved " +i + "" + touch.deltaPosition);
-                break;
-            case TouchPhase.Ended:
-                Debug.Log("Ended " +i + "" + touch.position);
-                break;
-            case TouchPhase.Canceled:
-            Debug.Log("Canceled " +i + "" + touch.position);
-                break;
-       }
+//        for (int i = 0; i < touches.Length; i++)
+//        {
+        
+//          var touch = touches[i];
 
-       }
-    }
-}
+//          switch (touch.phase)
+//        {
+//             case TouchPhase.Began:
+//                 Debug.Log("BEgan " + i + "" + touch.position);
+//                 break;
+//             case TouchPhase.Stationary:
+//                 Debug.Log("Stationary " + i + "" + touch.position);
+//                 break;
+//             case TouchPhase.Moved:
+//                 Debug.Log("Moved " +i + "" + touch.deltaPosition);
+//                 break;
+//             case TouchPhase.Ended:
+//                 Debug.Log("Ended " +i + "" + touch.position);
+//                 break;
+//             case TouchPhase.Canceled:
+//             Debug.Log("Canceled " +i + "" + touch.position);
+//                 break;
+//        }
+
+//        }
+//     }
+// }
