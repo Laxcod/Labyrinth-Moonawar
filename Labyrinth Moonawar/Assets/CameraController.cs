56767e1f9007c91915deb449120e69a4ba488f86
Laxcod <90342920+Laxcod@users.noreply.github.com>
2023-05-20 14:40:08 +0700

    Add Labyrint And Ball
    Menambahkan Labyrint dan bola pada permainan ini

--- Labyrinth Moonawar/Assets/CameraController.cs
+++ Labyrinth Moonawar/Assets/CameraController.cs
@@ -1,25 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Cinemachine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-    [SerializeField] CinemachineFreeLook CMFreelook;
-    public void SetInputActive(bool value)
-    {
-        if(value)
-        {
-            CMFreelook.m_XAxis.m_InputAxisName = "Mouse X";
-            CMFreelook.m_YAxis.m_InputAxisName = "Mouse Y";
-        }
-        else
-        {
-            CMFreelook.m_XAxis.m_InputAxisName = "";
-            CMFreelook.m_YAxis.m_InputAxisName = "";
+// public class CameraController : MonoBehaviour
+// {
+//     [SerializeField] CinemachineFreeLook CMFreelook;
+//     public void SetInputActive(bool value)
+//     {
+//         if(value)
+//         {
+//             CMFreelook.m_XAxis.m_InputAxisName = "Mouse X";
+//             CMFreelook.m_YAxis.m_InputAxisName = "Mouse Y";
+//         }
+//         else
+//         {
+//             CMFreelook.m_XAxis.m_InputAxisName = "";
+//             CMFreelook.m_YAxis.m_InputAxisName = "";
             
-            CMFreelook.m_XAxis.m_InputAxisValue = 0;
-            CMFreelook.m_YAxis.m_InputAxisValue = 0;
-        }
-    }
-}
+//             CMFreelook.m_XAxis.m_InputAxisValue = 0;
+//             CMFreelook.m_YAxis.m_InputAxisValue = 0;
+//         }
+//     }
+// }
